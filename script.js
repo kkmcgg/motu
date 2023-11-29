9d080f47374f478232c02d3bda0c5850984e0953
Kevin Mcguigan <kevin.mcguigan@nscc.ca>
2023-11-29 17:24:25 -0400

    added wms popup

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ var map = new ol.Map({
 document.getElementById('layer-opacity').addEventListener('input', function() {
     map.getLayers().item(0).setOpacity(parseFloat(this.value));
 });
+
+document.getElementById('plus-button').addEventListener('click', function() {
+    document.getElementById('popup-form').style.display = 'block';
+});
+
+document.getElementsByClassName('close-btn')[0].addEventListener('click', function() {
+    document.getElementById('popup-form').style.display = 'none';
+});
+
+document.getElementById('wms-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    var wmsUrl = document.getElementById('wms-url').value;
+    var layerName = document.getElementById('layer-name').value;
+    // Add your logic here to use the WMS URL and layer name
+    console.log("WMS URL: " + wmsUrl + ", Layer Name: " + layerName);
+
+    // Close the popup after submission
+    document.getElementById('popup-form').style.display = 'none';
+});
